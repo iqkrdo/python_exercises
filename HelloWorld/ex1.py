418a2c8f45f0dec5cdc5e37ec540baf1292ebe2f
Ionut P <ionutmpopa@gmail.com>
2025-03-05 00:30:10 +0200

    print hello

--- HelloWorld/ex1.py
+++ HelloWorld/ex1.py
@@ -2,12 +2,15 @@
 def sum_even_nb():
     print("\nSum of even x numbers! Enter numbers. Enter anything else to finish.\n")
     SumEvenNumbers = 0
+    ProductOddNumbers = 1
     try:
         while (1):
             number = int(input("Number: "))
             if number % 2 == 0: SumEvenNumbers += number
+            else: ProductOddNumbers *= number
     except:
-        print(f"The sum is {SumEvenNumbers}")
+        print(f"The sum of even numbers is {SumEvenNumbers}")
+        print(f"The product of odd numbers is {SumEvenNumbers}")
 
 
 if __name__ == '__main__':
